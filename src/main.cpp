028cfad8ee07678a6083b4f9cbcfac6ec72a58ef
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2021-12-23 12:51:43 -0500

    pre homie rework

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <list>
 #include <iostream>
 #include <memory>
+#include <homie.hpp>
 
 
 extern "C" {
@@ -58,6 +59,52 @@ extern char* build_timestamp;
 }
 
 using namespace garage;
+
+static void test_homie() {
+    homie::Device *d = new homie::Device("device123", "My Device");
+    d->setIpAddr("192.168.1.39");
+
+    homie::Node *dht22Node  = new homie::Node(d, "dht22", "DHT22 Temp/RH Sensor", "DHT22");
+    d->addNode(dht22Node);
+
+    auto tempProp = new homie::Property(dht22Node, "tempf", "Temperature in Fahrenheit", homie::FLOAT, false);
+    tempProp->setUnit(homie::DEGREE_SYMBOL + "F");
+    float tempF = 72.0;
+    tempProp->setValue(tempF);
+    dht22Node->addProperty(tempProp);
+
+    auto rhProp = new homie::Property(dht22Node, "rh", "Temperature in Fahrenheit", homie::FLOAT, false);
+    rhProp->setUnit("%");
+    float rh = 61.0;
+    rhProp->setValue(rh);
+    dht22Node->addProperty(rhProp);
+
+    auto doorNode = new homie::Node(d, "doora", "South Garage Door", "door");
+    d->addNode(doorNode);
+
+    auto openProp = new homie::Property(doorNode, "isopen", "Is Door Open", homie::BOOLEAN, false);
+    doorNode->addProperty(openProp);
+
+    auto relayProp = new homie::Property(doorNode, "relay", "Door Activator", homie::INTEGER, true);
+    doorNode->addProperty(relayProp);
+
+    auto introduction = new std::list<homie::Message *>;
+    d->introduce(introduction);
+    for (auto msg : *introduction) {
+        std::cout << msg->topic 
+        << "[q:" << msg->qos << ",r:" << msg->retained << ']' 
+        << " → " 
+        << msg->payload << std::endl;
+    }
+    delete introduction;
+    delete relayProp;
+    delete openProp;
+    delete rhProp;
+    delete tempProp;
+    delete dht22Node;
+    delete doorNode;
+    delete d;
+}
 
 static void sys_ip_cb(struct mg_rpc *c, void *cb_arg, struct mg_rpc_frame_info *fi, struct mg_str result, int error_code, struct mg_str error_msg) {
   Device *device = (Device*) cb_arg;
@@ -300,5 +347,7 @@ enum mgos_app_init_result mgos_app_init(void) {
   auto ai = std::make_shared<int>();
   *ai = 31337;
   std::cout << "shared ptr deref: " << *ai << std::endl;
+
+  test_homie();
   return MGOS_APP_INIT_SUCCESS;
 }
