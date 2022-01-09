b9a60d7e302019dae23c5e3c6a5765e13376d52b
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2022-01-09 17:03:13 -0500

    format

--- src/main.cpp
+++ src/main.cpp
@@ -32,56 +32,6 @@
 
 using namespace garage;
 
-static void network_config_rpc_cb(struct mg_rpc *c, void *cb_arg, struct mg_rpc_frame_info *fi, struct mg_str result, int error_code, struct mg_str error_msg)
-{
-  Device *device = (Device *)cb_arg;
-  LOG(LL_INFO, ("error code: %d", error_code));
-  char *ip = NULL;
-  char *mac = NULL;
-  char *id = NULL;
-  int scan_result = json_scanf(result.p, result.len, "{id: %Q, mac: %Q, wifi: {sta_ip: %Q}}", &id, &mac, &ip);
-
-  if (id)
-  {
-    LOG(LL_DEBUG, ("id: %s", id));
-    free(id);
-  }
-  LOG(LL_DEBUG, ("jsonf_scan result: %d", scan_result));
-  if (scan_result < 0)
-  {
-    LOG(LL_ERROR, ("json scanf error"));
-  }
-  else if (0 == scan_result)
-  {
-    LOG(LL_ERROR, ("json scanf keys not found"));
-  }
-  else
-  {
-    if (ip)
-    {
-      LOG(LL_INFO, ("ip: %s", ip));
-      std::string str = std::string(ip);
-      device->setIpAddr(str);
-      device->homieDevice->setLocalIp(str);
-      free(ip);
-    }
-    else
-    {
-      LOG(LL_ERROR, ("json_scanf failed to find ip address"));
-    }
-    if (mac)
-    {
-      LOG(LL_INFO, ("mac: %s", mac));
-      auto str = std::string(mac);
-      device->homieDevice->setMac(str);
-      free(mac);
-    }
-    else
-    {
-      LOG(LL_ERROR, ("json_scanf failed to find mac address"));
-    }
-  }
-}
 
 static void activate_sub_handler(struct mg_connection *nc, const char *topic,
                                  int topic_len, const char *msg, int msg_len,
@@ -478,11 +428,13 @@ static void sys_ready_cb(int ev, void *ev_data, void *userdata)
   static int callCount = 1;
   if (device->homieDevice && device->homieDevice->getLocalIp().length() == 0)
   {
+    net_inquire_config(device->homieDevice);
     LOG(LL_DEBUG, ("Inquiring network config %d", callCount));
     struct mg_rpc_call_opts opts = {.dst = mg_mk_str(MGOS_RPC_LOOPBACK_ADDR)};
     mg_rpc_callf(mgos_rpc_get_global(), mg_mk_str("Sys.GetInfo"), network_config_rpc_cb, device, &opts, NULL);
     callCount++;
   }
+
 
   device->homieDevice->setLifecycleState(homie::READY);
   if (mgos_sys_config_get_homie_enable())
